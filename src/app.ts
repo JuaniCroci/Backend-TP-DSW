f066e20864f65df2b2a02d73d243386c0abcef65
juani Croci <juanicroci.jc@gmail.com>
2026-05-02 18:21:56 -0300

    API Express

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+
+app.use('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000' )
+}) 
+
